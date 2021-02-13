eab5d3308e87aadd8da8f0462e53691d8bff4074
Fabian Schulz <f.schulz6113@gmail.com>
2021-02-13 23:57:43 +0100

    kalender: Formatiere gesamten Kalender
    Design wurde vollständig dem jetzigem angepasst.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 create_file() {
-  cat themes/"$1".css general.css hauptseite.css mein-unterricht/general.css mein-unterricht/aktuelle-eintraege.css mein-unterricht/kursmappen-uebersicht.css mein-unterricht/anwesenheit.css mein-unterricht/kursmappen-details.css nachrichten.css stundenplan.css popups.css login-logout/login.css login-logout/logout.css login-logout/nicht-freigeschaltet.css login-logout/sitzung-beendet.css >../build/schulportal-"$1".css
+  cat themes/"$1".css general.css hauptseite.css mein-unterricht/general.css mein-unterricht/aktuelle-eintraege.css mein-unterricht/kursmappen-uebersicht.css mein-unterricht/anwesenheit.css mein-unterricht/kursmappen-details.css nachrichten.css stundenplan.css kalender.css popups.css login-logout/login.css login-logout/logout.css login-logout/nicht-freigeschaltet.css login-logout/sitzung-beendet.css >../build/schulportal-"$1".css
   cd ../build || exit
   # Replaces lines starting with "/" with a new line ("d").  "|" is the sed command separator
   sed '\|/|d' schulportal-"$1".css >tempfile && mv tempfile schulportal-"$1".css
