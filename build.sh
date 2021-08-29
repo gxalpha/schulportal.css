acae52b7b523cb9128927b7e2779e3a00ec0431d
gxalpha <beckmann.sebastian@outlook.de>
2021-08-29 21:52:29 +0200

    Lay groundwork for supporting multiple themes

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 create_file() {
-  cat variables.css general.css hauptseite.css mein-unterricht/general.css mein-unterricht/aktuelle-eintraege.css mein-unterricht/kursmappen-uebersicht.css mein-unterricht/anwesenheit.css mein-unterricht/kursmappen-details.css nachrichten.css stundenplan.css popups.css login-logout/login.css login-logout/logout.css login-logout/nicht-freigeschaltet.css login-logout/sitzung-beendet.css >../build/"$1".css
+  cat themes/"$1".css general.css hauptseite.css mein-unterricht/general.css mein-unterricht/aktuelle-eintraege.css mein-unterricht/kursmappen-uebersicht.css mein-unterricht/anwesenheit.css mein-unterricht/kursmappen-details.css nachrichten.css stundenplan.css popups.css login-logout/login.css login-logout/logout.css login-logout/nicht-freigeschaltet.css login-logout/sitzung-beendet.css >../build/schulportal-"$1".css
   cd ../build || exit
   # Replaces lines starting with "/" with a new line ("d").  "|" is the sed command separator
-  sed '\|/|d' "$1".css >tempfile && mv tempfile "$1".css
+  sed '\|/|d' schulportal-"$1".css >tempfile && mv tempfile schulportal-"$1".css
   # Removes every empty line
-  sed '/^$/d' "$1".css >tempfile && mv tempfile "$1".css
+  sed '/^$/d' schulportal-"$1".css >tempfile && mv tempfile schulportal-"$1".css
+  cd ../src
 }
 
 mkdir build
 cd src || exit
-if [ "$1" = "test" ]; then
-  create_file testBuild
-else
-  create_file schulportal
-fi
+create_file light
+create_file dark
